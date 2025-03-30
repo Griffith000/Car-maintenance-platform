feca8b9910c278173d351f8bbceeae6e9f8c660e
Med Amine Slama <mohammedaminslama@gmail.com>
2025-03-30 16:51:58 +0100

    experiemental route.ts

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,19 +2,30 @@ import { PrismaClient } from '../generated/prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-static'
+
 const prisma = new PrismaClient().$extends(withAccelerate())
 
+// these are some test functions to get used to api calling in nextjs.
 export async function GET(request: Request) {
   console.log("testing this is a pain")
-  await prisma.user.create({
-    data: {
-      name: "Johhny",
-      email: "ahmedJohnny@gmail.com",
-    }
-  })
+  try {
+    await prisma.user.create({
+      data: {
+        name: "notSOJohnen",
+        email: "ahmedJohnny@gmail.com",
+      }
+    })
+  } catch {
+    console.log("welp very sad")
+    return NextResponse.json({
+      Message: "Error, we didn't add to the database, server error!"
+    }, { status: 500 })
+  }
   console.log("Added the user to the database")
-  return JSON.stringify({
-    Message: "Success!"
+  return NextResponse.json({
+    Message: "Successfully added to the database."
+
   })
 }
 // to figure out once i have set up the prisma orm
